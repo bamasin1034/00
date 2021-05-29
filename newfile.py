b9cab572c9c92ab41022ccecb9766084f95ba646
bamasin1034 <85021658+bamasin1034@users.noreply.github.com>
2021-05-29 13:49:19 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,43 @@
+print("901")
+def menu ( ):
+	option = input ("1 check your balance \n2.Transfer\n3.Airtime\n4.Transaction history")
+if (option =  "1"):
+print ("your acc bal is 325762 Naira
+print ("Enter 1 TO RETURN TO MENU")
+print ("ENTER 2 TO END")
+main = input (" ")
+if (main = = "1"):
+		menu ( )
+elif (main = = "2"):
+	 exit ( )
+else
+ print ("wroung input")
+elif ( option = = "2")
+acc = input ("Enter acct no you want to transfer money to")
+amount = input ("Enter amount")
+pin = input ("Enter four digit")
+confirm = input ("Do you want to send",amount",to this account number",acct"? /n1.Yes /n2 No")
+if ( comfirm= = "1")
+print ("Transaction successful")
+print ("ENTER 1 TO RETURN TO MENU /n ENTER 2 TO END")
+main =input (" ")
+if ( main == "1"):
+  menu ( )
+elif ( main = = "2")
+  exit ( )
+else 
+print ("wroung input")
+elif ( option = = "3"):
+reciever = input ( "1.for self /n2. for others")
+  if ( receiver = = "1")
+amount = input ("Enter amount")
+print ("Transaction successful")
+print ("ENTER 1 TO RETURN TO MENU /n ENTER 2 TO END")
+main = input (" ")
+  if ( main = = "1"):
+  	menu ( )
+elif ( main = = "2"):
+	 exit ( )
+else:
+ print ( option = = "4")
+print ("An sms would be sent to you shortly")
